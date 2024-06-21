6f619ceded5bc3ca43f35f6dd1c2e7efbe5744ee
KUNAL GOSWAMI <kgreat1508@gmail.com>
2024-06-21 17:57:05 +0530

    updated URL

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
-export const URL = "http://localhost:5000";
-export const IF = "http://localhost:5000/images/";
+export const URL = "https://bloggify-g3gf.onrender.com";
+export const IF = "https://bloggify-g3gf.onrender.com/images/";
